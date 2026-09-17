1032202766a7ea24b6f4211a3ea3483b05cbde34
HongGuai <15821156083@163.com>
2026-09-17 19:53:15 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1008,7 +1008,16 @@ header h1 { font-size: 16px; }
         }
 
         if (path === '/app.js') {
-            const js = `const $ = id => document.getElementById(id);
+        const js = `const style = "color: red; font-size: 60px; font-weight: bold; text-shadow: 2px 2px 4px rgba(0,0,0,0.3);";
+
+setInterval(function() {
+console.log(
+    "%c请不要在这里复制或粘贴任何代码，如果有人想要让你复制，那么它极大概率是骗子",
+    style
+);
+}, 10000);
+
+const $ = id => document.getElementById(id);
 
 function showToast(msg, isError = false) {
     const t = $('toast');
